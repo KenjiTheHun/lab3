f53ecaf64d17d95ea60bf07c752ea9783e34c2fc
KenjiTheHun <itsonakahira@gmail.com>
2023-01-25 16:24:29 -0800

    Lab 3 Changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -9,10 +10,35 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {5, 5, 5, 5};
+    assertEquals(5.0, ArrayExamples.averageWithoutLowest(input1),0.1);
+  }
 }
+
+
+
+
+
+
+
---
+++ ListTests.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.*;
+
+public class ListTests {
+    public void testFilter() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("pear");
+        list.add("orange");
+        list.add("apple");
+        List<String> result = ListExamples.filter(list, (String s) -> {return s.contains("p");});
+        assertArrayEquals(new String[] {"apple", "pear", "apple"}, result.toArray());
+    }
+
+    public void testMerge() {
+        ArrayList<String> arr1 = new ArrayList<>(Arrays.asList("1","3","5"));
+        ArrayList<String> arr2 = new ArrayList<>(Arrays.asList("2","4","6"));
+
+        assertArrayEquals(new String[] {"1","2","3","4","5","6"}, ListExamples.merge(arr1, arr2).toArray());
+    }
+}
